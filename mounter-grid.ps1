4edade0593c4117ac7d7ae055d79934986a6f571
David Delarosa <xdavidel@gmail.com>
2020-02-18 10:17:07 +0200

    changes and safeguards

--- mounter-grid.ps1
+++ mounter-grid.ps1
@@ -1,15 +1,28 @@
-# Grid with unmouted known partitions
-$chosen=Get-Partition | Where-Object -FilterScript { (-Not $_.DriveLetter) -and ($_.Type -notcontains "Unknown") } | Select-Object -Property PartitionNumber,Size,Type | Sort-Object -Property Size -Descending | Out-GridView -OutputMode "Single"
+$chosen=Get-Disk | ForEach-Object {
+    $diskprt = Get-Partition -DiskNumber $_.DiskNumber | Select-Object -Property PartitionNumber,Size,Type,DriveLetter
+    $diskprt | Add-Member -Name 'DiskName' -MemberType NoteProperty -Value $_.FriendlyName
+    $diskprt | Add-Member -Name 'DiskNumber' -MemberType NoteProperty -Value $_.DiskNumber
+    $diskprt | Add-Member -Name 'DiskSize' -MemberType NoteProperty -Value $_.Size
+    $diskprt | Add-Member -Name 'PartitionTable' -MemberType NoteProperty -Value $_.PartitionStyle
+
+    echo $diskprt
+
+} | Select-Object -Property DiskNumber,DiskName,DiskSize,PartitionNumber,DriveLetter,Type,Size,PartitionTable | 
+    Out-GridView -OutputMode "Single"
 
 # Safeguard
 if ($chosen -Eq $null) {exit}
+if ($chosen.DriveLetter) { echo "Hey! This partition is already mounted."; exit}
+if ($chosen.Type -contains "Unknown") { echo "Sorry, This partition has an unknown type."; exit}
 
 # Shoose available mounting drive letter
 $mnt=echo "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" | 
-    Where-Object -FilterScript { ( $_ -NotIn ((Get-PSDrive).Name -match '^[a-z]$')) } | Out-GridView -OutputMode "Single"
+    Where-Object -FilterScript { ( $_ -NotIn ((Get-PSDrive).Name -match '^[a-z]$')) } | 
+    Out-GridView -OutputMode "Single"
+
 
 # Safeguard
 if ($mnt -Eq $null) {exit}
 
 # mount the selected partition to the selected drive letter
-Get-Partition | Where-Object -FilterScript { (-Not $_.DriveLetter) -and ($_.PartitionNumber -Eq $chosen.PartitionNumber) -and ($_.Size -Eq $selected.Size) -and ($_.Type -notcontains "Unknown") } | Set-Partition -NewDriveLetter $mnt
+Get-Partition -DiskNumber $chosen.DiskNumber | Where-Object -FilterScript { ($_.PartitionNumber -Eq $chosen.PartitionNumber) } | Set-Partition -NewDriveLetter $mnt
